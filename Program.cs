d8ff5022c82c661c79dadb7963a4c83ca611902d
Shubham Kumar <shubham.7368@gmail.com>
2022-11-12 23:43:44 +0530

    Enabled for Production.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectio
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
