ebf185f4bc2329d569a78794142a87d2ae5f1231
Shubham Kumar <shubham.7368@gmail.com>
2022-11-13 00:14:19 +0530

    Rollbacked

--- Program.cs
+++ Program.cs
@@ -9,18 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1",new Microsoft.OpenApi.Models.OpenApiInfo { Title="BooksAPI",Version="v1"});
-    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-    {
-        Description = "JWT Authorization",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-});
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<BooksAPIDBContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -31,10 +20,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("swagger/v1/swagger.json","Books API");
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
